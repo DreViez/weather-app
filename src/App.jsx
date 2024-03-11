3bf1e90096bdb65e8c8b9064d8178736b73f830c
quajh <quajh123@gmail.com>
2024-03-11 14:12:15 -0400

    new changes

---
+++ src/App.jsx
@@ -0,0 +1,75 @@
+import React, { useEffect, useState } from "react";
+import TopButtons from "./components/topButtons";
+import Inputs from "./components/Inputs";
+import TimeAndLocation from "./components/TimeAndLocation";
+import TemperatureAndDetails from "./components/TemperatureAndDetails";
+import Forecast from "./components/Forecast";
+import { getFormattedWeatherData } from "./services/weatherService";
+
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const App = () => {
+  const [city, setCity] = useState("New York");
+  const [unit, setUnit] = useState("c");
+  const [weather, setWeather] = useState(null);
+
+  useEffect(() => {
+    const fetchWeather = async () => {
+      toast.info("Fetching weather for " + city);
+      await getFormattedWeatherData(city).then((data) => {
+        toast.success(`Fetched ${data.loc_name}, ${data.loc_country}`);
+        console.log(data);
+        setWeather(data);
+      });
+    };
+
+    fetchWeather();
+  }, [city, unit]);
+
+  
+const formatBackground = () => {
+  // Check if weather information is not available (falsy)
+  if (!weather)
+      // If weather information is not available, return default background gradient
+      return "from-cyan-700 to-blue-700";
+  // If weather information is available
+  // Check if it's daytime based on the value of weather.is_day
+  return weather.is_day
+      // If it's daytime, return a background gradient from yellow to orange
+      ? "from-yellow-300 to-orange-600"
+      // If it's nighttime, return a default background gradient
+      : "from-cyan-700 to-blue-700";
+};
+  return (
+    <div
+    
+      className={`max-w-screen-md mx-auto mt-4 py-5 px-32 bg-gradient-to-br ${formatBackground()} h-fit shadow-xl shadow-gray-400 rounded-lg`}
+    >
+      <TopButtons setCity={setCity} />
+      <Inputs setCity={setCity} unit={unit} setUnit={setUnit} />
+
+      {weather && (
+        <>
+          <TimeAndLocation weather={weather} />
+          <TemperatureAndDetails weather={weather} unit={unit} />
+
+          <Forecast
+            title="hourly forecast"
+            unit={unit}
+            forecast={weather.hourlyForecast}
+          />
+          <Forecast
+            title="daily forecast"
+            unit={unit}
+            forecast={weather.dailyForecast}
+          />
+        </>
+      )}
+
+      <ToastContainer autoClose={5000} theme="colored" newestOnTop={true} />
+    </div>
+  );
+};
+
+export default App;
